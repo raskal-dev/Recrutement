52bb0aae09b4e1a0b8fdfaf6cb7668695f40da2d
raskal-dev <kalvinrasamyzo@gmail.com>
2025-05-31 14:37:45 +0300

    feat(): add docker and promethus

--- src/Configs/Db.config.ts
+++ src/Configs/Db.config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const DbConfig: any = {
   HOST: process.env.DB_HOST ?? "localhost",
-  PORT: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  PORT: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   USER: process.env.DB_USER || "",
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_DATABASE,
